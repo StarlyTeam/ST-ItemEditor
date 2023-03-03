7a4f0ebfe7d11ae187db0db3bef8a83a59ef799c
yejunho10 <hyjcompany30@naver.com>
2023-03-03 20:02:31 +0900

    modify: TabCompleter

--- src/main/java/net/starly/itemeditor/command/tabcomplete/ItemEditorTab.java
+++ src/main/java/net/starly/itemeditor/command/tabcomplete/ItemEditorTab.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -15,76 +17,48 @@ public class ItemEditorTab implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+        List<String> tab = new ArrayList<>();
+
         if (args.length == 1) {
-            if (label.equalsIgnoreCase("ie")) {
-                return Arrays.asList("name", "cid", "type", "lore", "enchantment", "flag", "nbt");
-            } else {
-                return Arrays.asList("이름", "커스텀모델데이터", "타입", "로어", "인챈트", "플래그", "태그");
-            }
+            if (label.equalsIgnoreCase("ie")) tab.addAll(Arrays.asList("name", "cid", "type", "lore", "enchantment", "flag", "nbt"));
+            else tab.addAll(Arrays.asList("이름", "커스텀모델데이터", "타입", "로어", "인챈트", "플래그", "태그"));
         } else if (args.length == 2) {
-            if (Arrays.asList("커스텀모델데이터", "cid").contains(args[0].toLowerCase())) {
-                return Arrays.asList("1", "2", "3", "4", "5");
-            } else if (Arrays.asList("타입", "type").contains(args[0].toLowerCase())) {
-                return Arrays.asList(Arrays.stream(Material.values()).map(Enum::name).toArray(String[]::new));
-            }
+            if (Arrays.asList("커스텀모델데이터", "cid").contains(args[0].toLowerCase())) tab.addAll(Arrays.asList("1", "2", "3", "4", "5"));
+            else if (Arrays.asList("타입", "type").contains(args[0].toLowerCase())) tab.addAll(Arrays.asList(Arrays.stream(Material.values()).map(Enum::name).toArray(String[]::new)));
 
-            if (args[0].equalsIgnoreCase("로어")) {
-                return Arrays.asList("보기", "추가", "수정", "삭제", "초기화");
-            } else if (args[0].equalsIgnoreCase("lore")) {
-                return Arrays.asList("view", "add", "edit", "remove", "clear");
-            }
+            if (args[0].equalsIgnoreCase("로어")) tab.addAll(Arrays.asList("보기", "추가", "수정", "삭제", "초기화"));
+            else if (args[0].equalsIgnoreCase("lore")) tab.addAll(Arrays.asList("view", "add", "edit", "remove", "clear"));
 
-            if (args[0].equalsIgnoreCase("인챈트")) {
-                return Arrays.asList("보기", "추가", "삭제", "초기화");
-            } else if (args[0].equalsIgnoreCase("enchantment")) {
-                return Arrays.asList("view", "add", "remove", "clear");
-            }
+            if (args[0].equalsIgnoreCase("인챈트")) tab.addAll(Arrays.asList("보기", "추가", "삭제", "초기화"));
+            else if (args[0].equalsIgnoreCase("enchantment")) tab.addAll(Arrays.asList("view", "add", "remove", "clear"));
 
-            if (args[0].equalsIgnoreCase("플래그")) {
-                return Arrays.asList("보기", "추가", "삭제", "초기화");
-            } else if (args[0].equalsIgnoreCase("flag")) {
-                return Arrays.asList("view", "add", "remove", "clear");
-            }
+            if (args[0].equalsIgnoreCase("플래그")) tab.addAll(Arrays.asList("보기", "추가", "삭제", "초기화"));
+            else if (args[0].equalsIgnoreCase("flag")) tab.addAll(Arrays.asList("view", "add", "remove", "clear"));
 
-            if (args[0].equalsIgnoreCase("태그")) {
-                return Arrays.asList("보기", "설정", "삭제", "초기화");
-            } else if (args[0].equalsIgnoreCase("nbt")) {
-                return Arrays.asList("view", "set", "remove", "clear");
-            }
+            if (args[0].equalsIgnoreCase("태그")) tab.addAll(Arrays.asList("보기", "설정", "삭제", "초기화"));
+            else if (args[0].equalsIgnoreCase("nbt")) tab.addAll(Arrays.asList("view", "set", "remove", "clear"));
         } else if (args.length == 3) {
             if (Arrays.asList("로어", "lore").contains(args[0].toLowerCase())) {
-                if (Arrays.asList("보기", "view", "수정", "edit", "삭제", "remove").contains(args[1].toLowerCase())) {
-                    return Arrays.asList("1", "2", "3", "4", "5");
-                }
+                if (Arrays.asList("보기", "view", "수정", "edit", "삭제", "remove").contains(args[1].toLowerCase())) tab.addAll(Arrays.asList("1", "2", "3", "4", "5"));
             } else if (Arrays.asList("인챈트", "enchantment").contains(args[0].toLowerCase())) {
-                if (Arrays.asList("추가", "add", "삭제", "remove").contains(args[1].toLowerCase())) {
-                    return Arrays.asList(Arrays.stream(Enchantment.values()).map(s -> s.getKey().getKey()).toArray(String[]::new));
-                }
+                if (Arrays.asList("추가", "add", "삭제", "remove").contains(args[1].toLowerCase())) tab.addAll(Arrays.asList(Arrays.stream(Enchantment.values()).map(s -> s.getKey().getKey()).toArray(String[]::new)));
             } else if (Arrays.asList("플래그", "flag").contains(args[0].toLowerCase())) {
                 if (Arrays.asList("추가", "add", "삭제", "remove").contains(args[1].toLowerCase())) {
                     return Arrays.asList(Arrays.stream(ItemFlag.values()).map(Enum::name).toArray(String[]::new));
                 }
             } else if (Arrays.asList("태그", "nbt").contains(args[0].toLowerCase())) {
-                if (Arrays.asList("설정", "삭제").contains(args[1].toLowerCase())) {
-                    return Collections.singletonList("<키>");
-                } else if (Arrays.asList("set", "remove").contains(args[1].toLowerCase())) {
-                    return Collections.singletonList("<Key>");
-                }
+                if (Arrays.asList("설정", "삭제").contains(args[1].toLowerCase())) tab.add("<키>");
+                else if (Arrays.asList("set", "remove").contains(args[1].toLowerCase())) tab.add("<Key>");
             }
         } else if (args.length == 4) {
             if (Arrays.asList("인챈트", "enchant").contains(args[0].toLowerCase())) {
-                if (Arrays.asList("추가", "add").contains(args[1].toLowerCase())) {
-                    return Arrays.asList("1", "2", "3", "4", "5");
-                }
+                if (Arrays.asList("추가", "add").contains(args[1].toLowerCase())) tab.addAll(Arrays.asList("1", "2", "3", "4", "5"));
             } else if (Arrays.asList("태그", "nbt").contains(args[0].toLowerCase())) {
-                if (Arrays.asList("설정", "삭제").contains(args[1].toLowerCase())) {
-                    return Collections.singletonList("<값>");
-                } else if (Arrays.asList("set", "remove").contains(args[1].toLowerCase())) {
-                    return Collections.singletonList("<Value>");
-                }
+                if (Arrays.asList("설정", "삭제").contains(args[1].toLowerCase())) tab.add("<값>");
+                else if (Arrays.asList("set", "remove").contains(args[1].toLowerCase())) tab.add("<Value>");
             }
         }
 
-        return Collections.emptyList();
+        return StringUtil.copyPartialMatches(args[args.length - 1], tab, new ArrayList<>());
     }
 }
