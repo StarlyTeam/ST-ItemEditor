a009439e5127e69a3bcde8395d89a23e5706dde3
yejunho10 <hyjcompany30@naver.com>
2023-04-02 10:44:14 +0900

    fix: Bugs modify: message

--- src/main/java/net/starly/itemeditor/ItemEditorMain.java
+++ src/main/java/net/starly/itemeditor/ItemEditorMain.java
@@ -11,9 +11,9 @@ public class ItemEditorMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if (Bukkit.getPluginManager().getPlugin("ST-Core") == null) {
+        if (!isPluginEnabled("net.starly.core.StarlyCore")) {
             Bukkit.getLogger().warning("[" + plugin.getName() + "] ST-Core 플러그인이 적용되지 않았습니다! 플러그인을 비활성화합니다.");
-            Bukkit.getLogger().warning("[" + plugin.getName() + "] 다운로드 링크 : &fhttps://discord.gg/TF8jqSJjCG");
+            Bukkit.getLogger().warning("[" + plugin.getName() + "] 다운로드 링크 : &fhttp://starly.kr/");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
@@ -22,11 +22,20 @@ public class ItemEditorMain extends JavaPlugin {
         new Metrics(this, 17173);
 
         // COMMAND
-        Bukkit.getPluginCommand("itemeditor").setExecutor(new ItemEditorCmd());
-        Bukkit.getPluginCommand("itemeditor").setTabCompleter(new ItemEditorTab());
+        Bukkit.getPluginCommand("item-editor").setExecutor(new ItemEditorCmd());
+        Bukkit.getPluginCommand("item-editor").setTabCompleter(new ItemEditorTab());
     }
 
     public static JavaPlugin getPlugin() {
         return plugin;
+    }
+
+    private boolean isPluginEnabled(String path) {
+        try {
+            Class.forName(path);
+            return true;
+        } catch (NoClassDefFoundError ignored) {
+        } catch (Exception ex) { ex.printStackTrace(); }
+        return false;
     }
 }
